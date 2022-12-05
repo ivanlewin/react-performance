de28384481fb456794eef6137b5ff1e2a6428cf5
Iván Lewin <57846638+ivanlewin@users.noreply.github.com>
2022-12-05 00:46:13 -0300

    03

--- src/exercise/03.js
+++ src/exercise/03.js
@@ -1,10 +1,10 @@
 // React.memo for reducing unnecessary re-renders
 // http://localhost:3000/isolated/exercise/03.js
 
-import * as React from 'react'
-import {useCombobox} from '../use-combobox'
-import {getItems} from '../workerized-filter-cities'
-import {useAsync, useForceRerender} from '../utils'
+import * as React from 'react';
+import { useCombobox } from '../use-combobox';
+import { getItems } from '../workerized-filter-cities';
+import { useAsync, useForceRerender } from '../utils';
 
 function Menu({
   items,
@@ -21,27 +21,26 @@ function Menu({
           getItemProps={getItemProps}
           item={item}
           index={index}
-          selectedItem={selectedItem}
-          highlightedIndex={highlightedIndex}
+          isSelected={selectedItem?.id === item.id}
+          isHighlighted={highlightedIndex === index}
         >
           {item.name}
         </ListItem>
       ))}
     </ul>
-  )
+  );
 }
 // 🐨 Memoize the Menu here using React.memo
+Menu = React.memo(Menu);
 
 function ListItem({
   getItemProps,
   item,
   index,
-  selectedItem,
-  highlightedIndex,
+  isSelected,
+  isHighlighted,
   ...props
 }) {
-  const isSelected = selectedItem?.id === item.id
-  const isHighlighted = highlightedIndex === index
   return (
     <li
       {...getItemProps({
@@ -54,41 +53,33 @@ function ListItem({
         ...props,
       })}
     />
-  )
+  );
 }
-// 🐨 Memoize the ListItem here using React.memo
+
+ListItem = React.memo(ListItem);
+// // 🐨 Memoize the ListItem here using React.memo
+// ListItem = React.memo(ListItem, (prevProps, nextProps) => (
+//   (prevProps.highlightedIndex === prevProps.index) === // was previously highlighted
+//   (nextProps.highlightedIndex === nextProps.index) // is currently highlighted
+// ));
 
 function App() {
-  const forceRerender = useForceRerender()
-  const [inputValue, setInputValue] = React.useState('')
+  const forceRerender = useForceRerender();
+  const [inputValue, setInputValue] = React.useState('');
 
-  const {data: allItems, run} = useAsync({data: [], status: 'pending'})
+  const { data: allItems, run } = useAsync({ data: [], status: 'pending' });
   React.useEffect(() => {
-    run(getItems(inputValue))
-  }, [inputValue, run])
-  const items = allItems.slice(0, 100)
+    run(getItems(inputValue));
+  }, [inputValue, run]);
+  const items = allItems.slice(0, 100);
 
-  const {
-    selectedItem,
-    highlightedIndex,
-    getComboboxProps,
-    getInputProps,
-    getItemProps,
-    getLabelProps,
-    getMenuProps,
-    selectItem,
-  } = useCombobox({
+  const { selectedItem, highlightedIndex, getComboboxProps, getInputProps, getItemProps, getLabelProps, getMenuProps, selectItem, } = useCombobox({
     items,
     inputValue,
-    onInputValueChange: ({inputValue: newValue}) => setInputValue(newValue),
-    onSelectedItemChange: ({selectedItem}) =>
-      alert(
-        selectedItem
-          ? `You selected ${selectedItem.name}`
-          : 'Selection Cleared',
-      ),
+    onInputValueChange: ({ inputValue: newValue }) => setInputValue(newValue),
+    onSelectedItemChange: ({ selectedItem }) => alert(selectedItem ? `You selected ${selectedItem.name}` : 'Selection Cleared',),
     itemToString: item => (item ? item.name : ''),
-  })
+  });
 
   return (
     <div className="city-app">
@@ -96,7 +87,7 @@ function App() {
       <div>
         <label {...getLabelProps()}>Find a city</label>
         <div {...getComboboxProps()}>
-          <input {...getInputProps({type: 'text'})} />
+          <input {...getInputProps({ type: 'text' })} />
           <button onClick={() => selectItem(null)} aria-label="toggle menu">
             &#10005;
           </button>
@@ -110,7 +101,7 @@ function App() {
         />
       </div>
     </div>
-  )
+  );
 }
 
 export default App
